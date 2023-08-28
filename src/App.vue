84a87866f102243018810c3efa36441b3ca9418c
Francisco Rosemberg <frvgomes@gmail.com>
2023-08-28 07:11:30 -0300

    Implementando o placar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,104 @@
 <template>
   <div>
-    <h1>
-      Microphones can be used not only to pick up sound, but also to project sound similar to a speaker.
+    <ScoreBoard :winCount="this.winCount" :loseCount="this.loseCount"/>
+    <h1 
+      v-html="this.question">
     </h1>
+    <template v-if="this.question">
+      <template v-for="(answer, index) in this.answers" v-bind:key="index">
+        <input 
+          :disabled="this.asnwerSunmitted"
+          type="radio" 
+          name="options" 
+          :value="answer"
+          v-model="this.chosenAnswer"
+        >
+        <label v-html="answer"></label><br>
+      </template>
+      <button v-if="!this.asnwerSunmitted" @click="this.submitAnswer()" class="send" type="button">Send</button>
 
-    <input type="radio" name="options" value="True">
-    <label>True</label><br>
-
-    <input type="radio" name="options" value="False">
-    <label>False</label><br>
-
-    <button class="send" type="button">Send</button>
+      <section v-if="this.asnwerSunmitted" class="result">
+        <h4 v-if="this.chosenAnswer == this.correctAnswer"
+          v-html="'&#9989; Parabéns, Resposta Correta.'">
+        </h4>
+        <h4 v-else
+         v-html="'&#10060; Desculpa, Resposta Errada. Resposta Correta: ' + this.correctAnswer">
+         
+        </h4>
+        <button class="send" @click="this.getNewQuestion()" type="button">Next Question</button>
+      </section>
+    </template>
   </div>
 </template>
 
+
 <script>
+import ScoreBoard from '@/components/ScoreBoard.vue'
 
 
 export default {
   name: 'App',
+  components:{
+    ScoreBoard,
+  },
+  data(){  
+    return {
+      question: undefined,
+      incorrectAnswers: undefined,
+      correctAnswer: undefined,
+      chosenAnswer: undefined,
+      asnwerSunmitted: false,
+      winCount: 0,
+      loseCount: 0,
+    }
+  },
 
+  computed:{
+    answers(){
+      let answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
+      answers.splice(Math.round(Math.random() * answers.length), 0, this.correctAnswer);
+      return answers;  
+    }
+  },
+
+  methods:{
+    submitAnswer(){
+      if(!this.chosenAnswer){
+        alert('Escolha uma das opções')
+      } else {
+        this.asnwerSunmitted = true
+        if (this.chosenAnswer === this.correctAnswer){
+          this.winCount++;
+        } else {
+          this.loseCount++;
+        }
+      }
+    },
+    
+    getNewQuestion() {
+      this.asnwerSunmitted = false;
+      this.chosenAnswer = undefined;
+      this.question = undefined;
+      
+      this.axios
+      .get('https://opentdb.com/api.php?amount=1&category=18')
+      .then((response) => {
+        this.question = response.data.results[0].question;
+        this.incorrectAnswers = response.data.results[0].incorrect_answers;
+        this.correctAnswer = response.data.results[0].correct_answer;
+        
+      })
+    }
+  },
+
+  created(){
+    this.getNewQuestion()
+  }
 }
+
 //https://opentdb.com/api.php?amount=1&category=18&difficulty=easy
+
 </script>
-
-
 
 <style lang="scss">
 #app {
